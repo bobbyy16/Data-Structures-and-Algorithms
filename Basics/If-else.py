180c8d4449efb1a082bc3509257e6e44f84bcd55
Abhishek k <bobbyyyyy.16@gmail.com>
2023-07-25 12:06:28 +0530

    IF-Else

---
+++ Basics/If-else.py
@@ -0,0 +1,16 @@
+# problem
+
+# https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=1
+
+
+from typing import *
+
+def compareIfElse(a: int, b: int):
+    # Write your code here
+    if a == b:
+        return "equal"
+    elif a > b:
+        return "greater"
+    else:
+        return "smaller"
+    pass
