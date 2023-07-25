ddfa4e47984180d3084be91c91f495801039c102
Abhishek k <bobbyyyyy.16@gmail.com>
2023-07-25 11:10:01 +0530

    Find character case

---
+++ Basics/Find-character-case.py
@@ -0,0 +1,14 @@
+# problem statement
+
+# https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+
+ch = input()
+
+if ch >= 'A' and ch <= 'Z':
+    print(1)
+elif ch >= 'a' and ch <= 'z':
+    print(0)
+else:
+    print(-1)
+
