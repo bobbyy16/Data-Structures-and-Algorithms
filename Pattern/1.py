7a61ff6ebae7b82c273b7fe0db3bbb6a35f10d75
Abhishek k <bobbyyyyy.16@gmail.com>
2023-09-05 11:29:57 +0530

    pattern 1

--- Pattern/1.py
+++ Pattern/1.py
@@ -3,10 +3,11 @@
 # *****
 # *****
 
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
 
-for i in range(0, 5):
-    for j in range(0, 4):
+for i in range(rows):
+    for j in range(columns):
         print('*', end=" ")
     print()
-
     # end= " " -> makes all the astreks to print on the same line and next line print is used to go to next line
