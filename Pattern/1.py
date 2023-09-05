a3f8c990cd2db33581a85887be525bc3558d239c
Abhishek k <bobbyyyyy.16@gmail.com>
2023-09-05 11:08:31 +0530

    pattern 1

---
+++ Pattern/1.py
@@ -0,0 +1,12 @@
+# *****
+# *****
+# *****
+# *****
+# *****
+
+for i in range(0, 4):
+    for j in range(0, 4):
+        print('*', end=" ")
+    print()
+
+    # end= " " -> makes all the astreks to print on the same line and next line print is used to go to next line
