1cdbcff8b3013412136e34c4a50a1c47f02f4bec
Abhishek k <bobbyyyyy.16@gmail.com>
2023-09-06 11:03:44 +0530

    pattern 12

---
+++ Pattern/12.py
@@ -0,0 +1,21 @@
+#      *
+#      * *
+#      * * *
+#      * * * *
+#      * * *
+#      * *
+#      *
+
+n = int(input("Enter the number: "))
+
+# Upper half of the pattern
+for i in range(n):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+# Lower half of the pattern
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
