24999620936a1b6337c9a92aba988fb52d004dcd
Abhishek k <bobbyyyyy.16@gmail.com>
2023-09-06 11:25:48 +0530

    pattern 13

--- Pattern/13.py
+++ Pattern/13.py
@@ -8,10 +8,10 @@ n = int(input("Enter the number: "))
 
 start = 0
 for i in range(n):
-    if i % 2 == 0:
+    if i % 2 == 0: # every even row starts with 0
         start = 0
     else:
-        start = 1
+        start = 1 # every odd row starts with 1
     for j in range(i):
         print(start, end="")
         start = 1 - start   # if i equals to 1 in else: start = 1, it remains start = 1, so we are defining start = 1 - start
