0547f7b224ccb72584e6857a0f9a85d73c90d56b
Abhishek k <bobbyyyyy.16@gmail.com>
2023-09-06 11:08:00 +0530

    pattern 12

--- Pattern/12.py
+++ Pattern/12.py
@@ -15,7 +15,7 @@ for i in range(n):
     print()
 
 # Lower half of the pattern
-for i in range(n, 0, -1):
+for i in range(n, 0, -1): #n - start index, till 0, -1 is iteration(-1 in every row)
     for j in range(i):
         print("*", end=" ")
     print()
