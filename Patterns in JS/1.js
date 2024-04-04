635601fedc5e95b79e126fc34e1b05a9e1f0b65c
Abhishek k <bobbyyyyy.16@gmail.com>
2024-04-04 21:11:45 +0530

    one.js

--- Patterns in JS/1.js
+++ Patterns in JS/1.js
@@ -1,8 +1,21 @@
-const stdout = require('node:process');
+/*
+ 
+ * * * * *
+ * * * * *
+ * * * * * 
+ * * * * *
+ * * * * * 
+ 
+ */
 
-for (i = 0; i < 5; i++){
-    for(j = 0; j <= i; j++){
-        process.stdout.write('*')
+function one(n) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+      row = row + "*";
     }
-    process.stdout.write('\n')
+    console.log(row);
+  }
 }
+
+one(5);
