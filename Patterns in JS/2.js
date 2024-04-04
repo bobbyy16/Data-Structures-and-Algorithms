ec8be4679d0d9ce87f4e15d7cad1eac924dfced9
Abhishek k <bobbyyyyy.16@gmail.com>
2024-04-04 21:17:17 +0530

    triangle pattern

--- Patterns in JS/2.js
+++ Patterns in JS/2.js
@@ -1,26 +1,3 @@
-
-const n = 5;
-
-for (let i = 1; i <= n; i++) {
-    let spaces = " ".repeat(n - i);
-    let asterisks = "*".repeat(2 * i - 1);
-    console.log(spaces + asterisks + spaces);
-}
-// or
-
-for (let i = 1; i <= n; i++) {
-    for (let spaces = 0; spaces < n - i; spaces++) {
-        process.stdout.write(" ");
-    }
-    for (let asterisks = 0; asterisks < 2 * i - 1; asterisks++) {
-        process.stdout.write("*");
-    }
-    for (let spaces = 0; spaces < n - i; spaces++) {
-        process.stdout.write(" ");
-    }
-    console.log(); 
-}
-
 /*
    *   
   ***  
@@ -33,5 +10,21 @@ spaces   stars    spaces
   0        7       0
 
   spaces descending order = n - i
-  stars increasing odd numbers = 2 * i - 1
+  stars increasing odd numbers = 2 * i + 1
 */
+
+function one(n) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n - i; j++) {
+      row += " ";
+    }
+    for (let k = 0; k < 2 * i + 1; k++) {
+      row += "*";
+    }
+
+    console.log(row);
+  }
+}
+
+one(5);
